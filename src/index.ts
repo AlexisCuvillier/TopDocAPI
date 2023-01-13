87e97b9723d7324d179896ea00609853b81d0fb6
AlexisCuvillier <alexis.cuvillier@protonmail.com>
2023-01-13 08:01:22 +0100

    testDocker45

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,5 @@ createServer().then(app => {
     sequelize.sync({force : true})
 
     app.listen(process.env.PORT, () => logger.info(`Lancé sur le port ${process.env.PORT}`))
+    app.get('/hello', (req, res) => res.send('Hello Simplon by gaetan'));
 })
